0ec3ae6c46caa8c7529f54f8344ffc16f7761b4a
Doni0414 <nurda.agabek@mail.ru>
2025-05-08 03:41:06 +0500

    fix: images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const nextConfig = {
     images: {
       domains: ["localhost", "cdn-cjmik.nitrocdn.com", "play-lh.googleusercontent.com", "encrypted-tbn0.gstatic.com", "cs13.pikabu.ru"],
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '213.109.146.203',
+          port: '8080',
+          pathname: '/api/resources/**',
+        },
+      ],
     },
   };
   
